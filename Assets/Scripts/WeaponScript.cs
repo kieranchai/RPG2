c1fb60b6c196cfb5ae2342cbd3ccfe08132048b8
kieran <mayork383@gmail.com>
2023-06-20 15:40:19 +0800

    Added attack

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -7,12 +7,20 @@ public class WeaponScript : MonoBehaviour
     public int weaponId;
     public string weaponName;
     public int attackPower;
+    public string spritePath;
 
     private void Awake()
     {
         this.weaponId = 0;
         this.weaponName = "HANDS";
         this.attackPower = 1;
+        this.spritePath = "Sprites/Circle";
+
+        SpriteRenderer weaponSprite = gameObject.GetComponent<SpriteRenderer>();
+        Sprite sprite = Resources.Load<Sprite>(this.spritePath);
+        weaponSprite.sprite = sprite;
+
+        RefreshColliders();
     }
 
     public void setWeaponData(Weapon weaponData)
@@ -20,11 +28,19 @@ public class WeaponScript : MonoBehaviour
         this.weaponId = weaponData.weaponId;
         this.weaponName = weaponData.weaponName;
         this.attackPower = weaponData.attackPower;
+
+        RefreshColliders();
     }
 
     public void doAttack()
     {
-
         Debug.Log("Damage for " + this.attackPower);
+    }
+
+    private void RefreshColliders()
+    {
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+        GetComponent<PolygonCollider2D>().isTrigger = true;
     }
 }
